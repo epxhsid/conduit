0e529ee8800962314907f3e89c0aad9a99635918
epxhsid <joaqu26@proton.me>
2026-02-05 08:35:08 +0800

    change formatting for rootCmd to align syntax

--- cmd/root.go
+++ cmd/root.go
@@ -8,8 +8,8 @@ import (
 )
 
 var rootCmd = &cobra.Command{
-	Use:   "conduit",
-	Short: "Expose a local port to a public domain",
+	Use:   `conduit`,
+	Short: `Expose a local port to a public domain`,
 	Long:  `A CLI tool to expose local services behind NAT over a multiplexed TCP tunnel`,
 	Args:  cobra.ExactArgs(2),
 }

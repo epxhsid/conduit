a827d8ae9687d9baed028a35766385e6c7a2221f
epxhsid <joaqu26@proton.me>
2026-02-15 20:10:13 +0800

    refactor: restructure Registry type and initialization in router package

--- core/router/router.go
+++ core/router/router.go
@@ -1,4 +1,18 @@
 package router
 
+import (
+	"sync"
+
+	"github.com/hashicorp/yamux"
+)
+
 type Registry struct {
+	sync.RWMutex
+	Streams map[string]*yamux.Stream
+}
+
+func NewRegistry() *Registry {
+	return &Registry{
+		Streams: make(map[string]*yamux.Stream),
+	}
 }

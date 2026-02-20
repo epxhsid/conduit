0dabebeb932344c8201e204b40bbbd70617e9db0
epxhsid <joaqu26@proton.me>
2026-02-20 23:23:25 +0800

    refactor: update counting writer to use exported fields for atomic operations

--- mux/writer.go
+++ mux/writer.go
@@ -6,12 +6,20 @@ import (
 )
 
 type CountingWriter struct {
-	n *atomic.Int64
-	w io.Writer
+	N atomic.Int64
+	W io.Writer
+}
+
+func NewCountingWriter(w io.Writer) *CountingWriter {
+	return &CountingWriter{W: w}
 }
 
 func (c *CountingWriter) Write(p []byte) (int, error) {
-	n, err := c.w.Write(p)
-	c.n.Add(int64(n))
+	n, err := c.W.Write(p)
+	c.N.Add(int64(n))
 	return n, err
+}
+
+func (c *CountingWriter) Count() int64 {
+	return c.N.Load()
 }
